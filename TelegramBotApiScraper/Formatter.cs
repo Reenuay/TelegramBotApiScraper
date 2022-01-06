762fdde2729e843904773d7fc1001ab8037d2ae0
Reenuay <reenuay777@gmail.com>
2022-01-07 02:43:58 +0400

    Rename project folder

---
+++ TelegramBotApiScraper/Formatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotApiScraper
+{
+    static internal class Formatter
+    {
+        static internal string UniformDescription(string description)
+        {
+            description = description
+                .Replace('<', '{')
+                .Replace('>', '}')
+                .Replace("\n", " ")
+                .Replace("  ", " ")
+                .Replace("\n\n", " ")
+                .Replace("Optional. ", "");
+
+            if (!description.EndsWith('.') && !description.EndsWith(':'))
+            {
+                description += ".";
+            }
+
+            return description;
+        }
+    }
+}
