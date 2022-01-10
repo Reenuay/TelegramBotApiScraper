adbfd3600736c9e12f8a659542d594a9c2736175
Reenuay <reenuay777@gmail.com>
2022-01-10 19:12:11 +0400

    Fix

--- TelegramBotApiScraper/Saver.cs
+++ TelegramBotApiScraper/Saver.cs
@@ -25,7 +25,7 @@ namespace TelegramBotApiScraper
                 path,
                 $"---\n{content}---\n"
                 + "```dataviewjs\n"
-                + "dv.view('./');\n"
+                + "dv.view('');\n"
                 + "```\n"
             );
         }
